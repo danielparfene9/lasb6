f2789a551add9e573ecfb1f8316021a87a67ac94
danielparfene9 <daniel.parfene@isa.utm.md>
2025-04-13 14:01:56 +0300

    add input and selector

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,20 @@
 <script>
+  let newTitle = '';
+  let newTag = 'general';
 </script>
 
 <main>
   <h1>Daily Wins Tracker</h1>
+
+  <input bind:value={newTitle} placeholder="What are your plans?" />
+  <select bind:value={newTag}>
+    <option value="general">General</option>
+    <option value="health">Health</option>
+    <option value="study">Study</option>
+    <option value="social">Social</option>
+  </select>
+  
+  <button>Add Win</button>
 </main>
 
 <style>
