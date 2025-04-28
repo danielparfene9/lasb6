7ee0e6a96c2e81e35a5f6cbe69030798095302a9
danielparfene9 <daniel.parfene@isa.utm.md>
2025-04-28 21:10:04 +0300

    add comma

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [svelte()],
-  base: '/lasb6/'
+  base: '/lasb6/',
 })
